a00f0afc5e017593ae031b63c66dd395ed9f7728
AlexBulgakov <alexbulgakov90@gmail.com>
2021-11-04 17:45:31 +0300

    update task

--- src/main/java/koschei/Application.java
+++ src/main/java/koschei/Application.java
@@ -10,6 +10,8 @@ public class Application {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         KoscheiTheDeathless koscheiTheDeathless =
                 applicationContext.getBean(KoscheiTheDeathless.class);
+
+        
         System.out.println(koscheiTheDeathless.getRulesByDeth());
     }
 }
