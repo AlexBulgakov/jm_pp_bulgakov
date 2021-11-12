9fb2ccdb18a3602031aca7ecf284bec41cd2708e
AlexBulgakov <alexbulgakov90@gmail.com>
2021-11-13 00:39:08 +0300

    got acquainted with the task 2.4.1

--- src/main/java/jm/security/example/config/AppInit.java
+++ src/main/java/jm/security/example/config/AppInit.java
@@ -1,10 +1,6 @@
-package webapplication.config;
+package jm.security.example.config;
 
-import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 
 public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
 
@@ -30,14 +26,4 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
         return new String[]{"/"};
     }
 
-    @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
-        super.onStartup(servletContext);
-        registerHiddenFieldFilter(servletContext);
-    }
-
-    private void registerHiddenFieldFilter(ServletContext context) {
-        context.addFilter("hiddenHttpMethodFilter",
-                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null, true, "/*");
-    }
 }
